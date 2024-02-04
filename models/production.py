da288f11f5463cd6bab13728c5c359184e5a8d83
arash <arash@Kimya.kimyagroup.org>
2024-02-04 01:44:41 -0500

    update

--- models/production.py
+++ models/production.py
@@ -20,6 +20,13 @@ class KmPetronadProductionRecordInherit(models.Model):
             rec_date = jdatetime.date(int(jdate[0]), int(jdate[1]), int(jdate[2]),).togregorian()
             record.write({'data_date': rec_date})
 
+    def amount_converter(self):
+        active_ids = self.env.context.get('active_ids')
+        production_records = self.browse(active_ids)
+        for record in production_records:
+            amount = -1 * record.amount if record.amount < 0 else record.amount
+            record.write({'amount': amount})
+
 
             # print(f'\n production_records: \n {record.j_data_date}  :  {rec_date}')
 
